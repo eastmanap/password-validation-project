8e9458886c31c6c74bf90f728a9532bdf530331f
eastmanap <eastmanap354@northwested.org>
2024-12-18 11:11:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def check_length():
         return
 
 print(check_length())
+
 # Check first five
 def check_first_five():
     if len(password) >= 5 and not password[:5].isalpha():
@@ -20,6 +21,7 @@ def check_first_five():
         return
 
 print(check_first_five())
+
 # Check last three
 def check_last_three():
     if len(password) >= 3 and not password[-3:].isdigit():
@@ -28,3 +30,37 @@ def check_last_three():
         return
 
 print(check_last_three())
+
+# Check for letters
+def check_for_letter():
+    for char in password:
+        if char.isalpha():
+            return
+        else:
+            return('Password must contain at least one letter!')
+
+print(check_for_letter)
+
+# Check for no special
+def check_no_special_chars():
+   
+    allowed_characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+    
+    for char in password:
+        if char not in allowed_characters:
+            return ('Password cannot contain special characters or symbols!')
+        else:
+            return
+
+print(check_no_special_chars())
+
+# Check for number
+
+def check_for_number(password):
+    for char in password:
+        if char.isdigit():
+            return
+        else:
+            return('Password must contain at least one number!')
+
+print(check_for_number())
